28cc8d81d0797d8636272cc808e37343ace905b2
abdelaziz aitmansour <mansour.aziz@hotmail.com>
2022-01-06 12:36:57 -0500

    End of section seven

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,62 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        // api/buggy/auth
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret test";
+        }
+
+        // api/buggy/not-found
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = this._context.Users.Find(-1);
+            if (thing==null)
+            {
+                return NotFound();
+            }
+            return Ok(thing);
+        }
+
+        // api/buggy/server-error
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            
+                var thing = this._context.Users.Find(-1);
+                var thingToReturn = thing.ToString();
+                return thingToReturn;
+           
+              
+            
+            
+        }
+
+        // api/buggy/server-error
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest( "this not a good request");
+        }
+    }
+}
